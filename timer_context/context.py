caa4296d1cc22204c97ba7dcad277bb2870f2586
qthequartermasterman <19497738+qthequartermasterman@users.noreply.github.com>
2025-01-22 06:52:47 +0000

    style: style fixes by ruff

--- timer_context/context.py
+++ timer_context/context.py
@@ -6,11 +6,11 @@ from typing import Any
 try:
     from typing import Self
 except ImportError:
-    try: 
+    try:
         from typing_extensions import Self
 
     except ImportError:
-        from typing import TypeAlias, TypeVar
+        from typing import TypeAlias
 
         Self: TypeAlias = "TimerContext"
 
@@ -20,20 +20,20 @@ class TimerContext:
 
     Attributes:
         start: The start time.
-        start_counter: A performance counter representing when the context 
-            began, in nanoseconds. This should only ever be compared against 
+        start_counter: A performance counter representing when the context
+            began, in nanoseconds. This should only ever be compared against
             another performance counter.
         end: The end time (if the context manager has exited).
-        end_counter: A performance counter representing when the context 
-            exited, in nanoseconds. This should only ever be compared against 
+        end_counter: A performance counter representing when the context
+            exited, in nanoseconds. This should only ever be compared against
             another performance counter.
         duration: The duration of the block of code. If the context manager has
             not exited, this will be the duration up to the current time.
 
     Note:
         Because of the additional overhead of the context manager, this may not
-        be the most accurate way to time a block of fast code, in the order of 
-        single nanoseconds. For a slow block of code, the overhead should be 
+        be the most accurate way to time a block of fast code, in the order of
+        single nanoseconds. For a slow block of code, the overhead should be
         proportionally small.
 
     Examples:
@@ -58,14 +58,14 @@ class TimerContext:
     def __enter__(self) -> Self:
         self.end = None
         self.end_counter = None
-        # Start the counter last because datetime.datetime.now() takes longer 
+        # Start the counter last because datetime.datetime.now() takes longer
         # than the perf_counter, and the counter needs to be more precise.
         self.start = datetime.datetime.now()
         self.start_counter = time.perf_counter_ns()
         return self
 
     def __exit__(self, *args: object, **kwargs: Any) -> None:
-        # End the counter first because datetime.datetime.now() takes longer 
+        # End the counter first because datetime.datetime.now() takes longer
         # than the perf_counter, and the counter needs to be more precise.
         self.end_counter = time.perf_counter_ns()
         self.end = datetime.datetime.now()
